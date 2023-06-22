569fb1d8f97b9c9636e91d1587039afe4de21aea
Ryan Baggs <ryanbaggs7@gmail.com>
2023-06-21 21:11:11 -0500

    Add info command. Refactor.
    Add info command to print bot information as discord message.
    
    Refactor main.py to use discord Bot instead of Client, and remove on_message event.

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import os
 import discord
+from discord.ext import commands
 
-# Login token for discord bot.
+# Login token.
 TOKEN = os.getenv('BROKE_PHI_BOT_TOKEN')
 
+# The API that the bot will need permissions for.
+# Privileged intents must be configured in app settings on:
+# https://discord.com/developers/applications
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Create the connection to discord.
-client = discord.Client(intents=intents)
+# Create a bot session and specify the prefix to commands.
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 # Events are asynchronous and code is executed as a callback.
 # Called when bot has finished logging in and setting things up.
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-# Called when bot has recieved a message.
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# Prints bot information as a discord message.
+@bot.command()
+async def info(ctx, cmd ='info'):
+    await ctx.send('''I am Broke Phi Bot, I ain\'t got it.
+    \nEnter the command you would like to do in the format:  $[command] [arguments]
+    Example: $info info
+    \nAdd quotes around multiple word arguments.
+    Example: $command \"Why do I like Dead by Daylight?\"
+    \nThe command $list gives a list of all commands.''')
 
 # Run the bot with login token.
-client.run(TOKEN)
+bot.run(TOKEN)
