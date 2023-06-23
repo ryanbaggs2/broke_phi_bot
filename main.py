27b6b910f6fc399267d03e19821b4df4688134cc
Ryan Baggs <ryanbaggs7@gmail.com>
2023-06-22 22:22:13 -0500

    Add GetAppList, sale command, and .gitignore.
    Add GetAppList steam API request to get the Apps in the steam store.
    
    Add sale command, which finds a name within the App List and sends a message with the name.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import discord
 from discord.ext import commands
+import requests
+import json
 
 # Login token.
 TOKEN = os.getenv('BROKE_PHI_BOT_TOKEN')
@@ -18,6 +20,12 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 # Called when bot has finished logging in and setting things up.
 @bot.event
 async def on_ready():
+    global content
+    with open('appdata', mode='w+') as f:
+        r = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
+        content = r.json()
+        f.write(json.dumps(content))
+    
     print(f'We have logged in as {bot.user}')
 
 # Prints bot information as a discord message.
@@ -29,6 +37,21 @@ async def info(ctx, cmd ='info'):
     \nAdd quotes around multiple word arguments.
     Example: $command \"Why do I like Dead by Daylight?\"
     \nThe command $list gives a list of all commands.''')
+
+@bot.command()
+async def sale(ctx, game):
+    apps = content['applist']
+
+    found = False
+    for app in apps['apps']:
+        if app['name'] == game:
+            found = True
+            break;
+ 
+    if found:
+        await ctx.send(f'Found {game}')
+    else:
+        await ctx.send(f'Did not find {game}')
 
 # Run the bot with login token.
 bot.run(TOKEN)
